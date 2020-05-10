b1c6005dc24568b7922e1f45b52a39e962c5e18c
hrvojeBencik <hrvojebencik1@gmail.com>
2020-05-10 17:29:50 +0200

    Version 1.0

--- script.py
+++ script.py
@@ -12,6 +12,11 @@ projectName = input()
 print("Creating Flutter project...")
 os.system("flutter create " + projectName)
 
+# Opening project folder in VS Code
+os.chdir(projectsFolder+"/"+projectName)
+print("Opening project folder in VS Code...")
+os.system("code .")
+
 # Going into project folder and then /lib folder
 os.chdir(projectsFolder+"/"+projectName+"/lib")
 
@@ -20,3 +25,9 @@ os.makedirs("widgets")
 print("Widgets folder made...")
 os.makedirs("pages")
 print("Pages folder made...")
+
+# Opening the iOS simulator and running the app
+print("Opening iOS simulator...")
+os.system("open -a Simulator.app")
+print("Running the app...")
+os.system("flutter run -d iPhone")
